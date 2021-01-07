19ca8b0d690d740a268a7103bba7c3876aab08fa
beau trepp <beautrepp@gmail.com>
2021-01-07 10:38:46 +0800

    Adds parser to get local file entries
    This takes the full input and returns a vector
    of the local file entries.
    
    This is useful for extracting the file data out

---
+++ zipr-nom/src/search/find_local_file_entries.rs
@@ -0,0 +1,70 @@
+use nom::{
+    IResult,
+};
+use zipr_core::data::{CentralDirectoryEntry, LocalFileEntry};
+
+use crate::data::parse_local_file;
+
+use super::find_central_directory_entries;
+
+fn local_entry<'a>(full_file: &'a[u8], directory: &CentralDirectoryEntry)
+            -> IResult<&'a [u8], LocalFileEntry<'a>> {
+                let start = directory.relative_offset as usize;
+                let end = full_file.len();
+                let local_bytes = &full_file[start..end];
+                let (rem,entry) = parse_local_file(local_bytes)?;
+                Ok((rem,entry))
+            }
+
+/// Given the full file. Finds all the local file entries
+/// Note this uses the central directory header to find the locations.
+/// So both must be valid/non-corrupt    
+pub fn find_local_file_entries<'a>(
+    input: &'a [u8],
+) -> IResult<&'a [u8], Vec<LocalFileEntry<'a>>> {
+    let (_, directories) = find_central_directory_entries(input)?;
+
+    // There should be a way to nicely do this with iterators, but trouble finding
+    // sequence (Vec<IResult> -> IResult<Vec<_>>) for nom.
+    let mut local = Vec::with_capacity(directories.len());
+    for directory in directories.iter() {
+        let (_,file) = local_entry(input, directory)?;
+        local.push(file);
+    }
+    Ok((&[],local))
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    use nom::Finish;
+
+    use super::*;
+    #[test]
+    fn hello_world_store_as_entries() {
+        let hello = include_bytes!("../../../assets/hello_world_store.zip");
+        let data = hello; 
+        let result = find_local_file_entries(data).finish();
+
+        let (rem, result) = result.unwrap();
+
+        assert_eq!(0, rem.len());
+        assert_eq!(1, result.len());
+        assert_eq!(Path::new("hello.txt"), result[0].file_name);
+    }
+
+    #[test]
+    fn two_files_store_as_entries() {
+        let hello = include_bytes!("../../../assets/two_files_store.zip");
+        let data = hello;
+        let result = find_local_file_entries(data).finish();
+
+        let (rem, result) = result.unwrap();
+
+        assert_eq!(0, rem.len());
+        assert_eq!(2, result.len());
+        assert_eq!(Path::new("hello.txt"), result[0].file_name);
+        assert_eq!(Path::new("moredata.txt"), result[1].file_name);
+    } 
+}
--- zipr-nom/src/search/mod.rs
+++ zipr-nom/src/search/mod.rs
@@ -1,9 +1,10 @@
-mod find_central_directory_entries;
 ///! Parsers that try to find items inside the zip file
 ///! This is required to enter the zip file.. as they require you to search
 ///! backwards, but other utilities for trying to find data are useful too
 ///! Note in this context we are parsing the 'full' file to the parsers
 mod find_end_of_central_directory;
-
+mod find_central_directory_entries;
+mod find_local_file_entries;
 pub use find_central_directory_entries::find_central_directory_entries;
 pub use find_end_of_central_directory::find_end_of_central_directory;
+pub use find_local_file_entries::find_local_file_entries;
